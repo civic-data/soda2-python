8294a138d1b4cff933a9326bdc42c9ebf155c2c2
localadmin <ralph@brooklynmarathon.com>
2014-05-06 15:13:57 -0400

    add 311 requests

--- nyc311/main.py
+++ nyc311/main.py
@@ -1,11 +1,26 @@
 from socratapython import socrata
+import credentials
 
 print vars(socrata)
 # this should really use OAuth 2.0
-reader = socrata.Socrata('https://data.cityofnewyork.us/','PLACE USER ID HERE','PASSWORD HERE','xC8nzr6tI03SZuDEPL1tx9WDW')
+#reader = socrata.Socrata('https://data.cityofnewyork.us/','PLACE USER ID HERE','PASSWORD HERE','PLACE TOKEN HERE')
+reader = socrata.Socrata('https://data.cityofnewyork.us/',credentials.Credentials.user,credentials.Credentials.pass1,credentials.Credentials.token)
 print vars(reader)
 print reader.setDataset('erm2-nwe9')
-items=reader.get()
-#print items
-for item in items:
-    print '%s %s' % (item['created_date'], item['complaint_type'])
+
+try:
+    #items=reader.get({'where': 'created_date >= 2014-05-04', 'limit':'2'})
+    #items=reader.get({'order': 'created_date desc', 'limit':'10'})
+    items=reader.get({'select': 'count(complaint_type) as count1,complaint_type', 'group':'complaint_type', 'order':'count1 desc'})
+    #items=reader.get({'limit': '2'})
+    #items=reader.get()
+except Exception,e:
+    print 'e1', e
+print items
+
+try:
+    for item in items:
+        #print item
+        print '%s %s %s %s' % (item['created_date'], item['complaint_type'],item['latitude'],item['longitude'])
+except Exception,e:
+    print 'e2', e
