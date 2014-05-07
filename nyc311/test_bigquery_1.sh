f141417f6e3072b63eb87b429100861b6e704d37
localadmin <ralph@brooklynmarathon.com>
2014-05-07 14:06:14 -0400

    performance test

---
+++ nyc311/test_bigquery_1.sh
@@ -0,0 +1,3 @@
+time bq query select Created_Date FROM [nyc.311] order by Created_Date asc limit 2
+time bq query select Created_Date FROM [nyc.311] order by Created_Date desc limit 2
+time bq query select count(*) FROM [nyc.311]
